30fa3fae8784590d8a9169a12eaefc7f102eea28
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-28 13:43:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,55 +10,69 @@ github_username = st.sidebar.text_input("Enter GitHub Username:")
 
 if github_username:
 
-    if st.sidebar.button('🧑 Fetch Profile'):
-        profile = conn.get_user_profile(github_username)
-        st.subheader(f"Profile of {github_username}")
-        st.dataframe(profile)
+    # Profile Information
+    profile = conn.get_user_profile(github_username)
+    st.image(profile['avatar_url'], width=100)
+    st.write(f"**Name:** {profile['name']}")
+    st.write(f"**Bio:** {profile['bio']}")
+    st.write(f"**Location:** {profile['location']}")
+    st.write(f"**Public Repositories:** {profile['public_repos']}")
+    st.write(f"**Followers:** {profile['followers']}")
+    st.write(f"**Following:** {profile['following']}")
 
-    if st.sidebar.button('📅 Fetch Recent Activity'):
-        activity = conn.get_user_activity(github_username)
-        st.subheader("Recent Activity")
-        st.dataframe(activity)
-
+    # Repositories
     if st.sidebar.button('📂 Fetch Repositories'):
         repos = conn.get_user_repositories(github_username)
         st.subheader("Repositories")
-        st.dataframe(repos)
+        st.write(repos)
 
+    # Recent Activity
+    if st.sidebar.button('📅 Fetch Recent Activity'):
+        activity = conn.get_user_activity(github_username)
+        st.subheader("Recent Activity")
+        st.write(activity)
+
+    # Issues
     if st.sidebar.button('🐞 Fetch Issues'):
         issues = conn.get_user_issues(github_username)
         st.subheader("Issues Created")
-        st.dataframe(issues)
+        st.write(issues)
 
+    # Pull Requests
     if st.sidebar.button('🔄 Fetch Pull Requests'):
         pull_requests = conn.get_user_pull_requests(github_username)
         st.subheader("Pull Requests Created")
-        st.dataframe(pull_requests)
+        st.write(pull_requests)
 
+    # Starred Repositories
     if st.sidebar.button('⭐ Fetch Starred Repos'):
         starred_repos = conn.get_user_starred_repos(github_username)
         st.subheader("Starred Repositories")
-        st.dataframe(starred_repos)
+        st.write(starred_repos)
 
+    # Followers
     if st.sidebar.button('👥 Fetch Followers'):
         followers = conn.get_user_followers(github_username)
         st.subheader("Followers")
-        st.dataframe(followers)
+        st.write(followers)
 
+    # Following
     if st.sidebar.button('👣 Fetch Following'):
         following = conn.get_user_following(github_username)
         st.subheader("Following")
-        st.dataframe(following)
+        st.write(following)
 
+    # Gists
     if st.sidebar.button('📜 Fetch Gists'):
         gists = conn.get_user_gists(github_username)
         st.subheader("Gists")
-        st.dataframe(gists)
+        st.write(gists)
 
+    # Organizations
     if st.sidebar.button('🏢 Fetch Organizations'):
         orgs = conn.get_user_organizations(github_username)
         st.subheader("Organizations")
-        st.dataframe(orgs)
+        st.write(orgs)
 
 else:
     st.sidebar.warning("Please enter a GitHub username.")
