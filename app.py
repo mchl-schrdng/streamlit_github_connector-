6333aa889efb9bdf03d1490650534ed115216400
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-28 10:50:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from src.github_connector import GitHubConnection
 # Create an instance of the GitHubConnection with a connection name
 conn = GitHubConnection("github")
 
-# Use the connection to fetch user's GitHub activity
-user_activity = conn.get_user_activity("mchl-schrdng")
-st.write(user_activity)
+# Add a button to the Streamlit app
+if st.button('Extract GitHub Activity'):
+    # Use the connection to fetch user's GitHub activity when the button is clicked
+    user_activity = conn.get_user_activity("your_github_username")
+    
+    # Display the result in Streamlit
+    st.dataframe(user_activity)
