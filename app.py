f6cafac859dc1856177af93503721db0662b6090
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-28 14:01:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,7 @@ if github_username:
     # Profile Information
     profile = conn.get_user_profile(github_username)
     st.image(profile['avatar_url'], width=100)
-    st.write(f"**Name:** {profile['name']}")
-    st.write(f"**Bio:** {profile['bio']}")
-    st.write(f"**Location:** {profile['location']}")
+    st.dataframe([profile])
 
     # Repositories
     if st.sidebar.button('📂 Fetch Repositories'):
@@ -44,7 +42,47 @@ if github_username:
         activity_dates_count = Counter(activity_dates)
         st.line_chart(activity_dates_count)
 
-    # ... [Rest of the code remains similar]
+    # Issues
+    if st.sidebar.button('🐞 Fetch Issues'):
+        issues = conn.get_user_issues(github_username)
+        st.subheader("Issues Created")
+        st.dataframe(issues)
+
+    # Pull Requests
+    if st.sidebar.button('🔄 Fetch Pull Requests'):
+        pull_requests = conn.get_user_pull_requests(github_username)
+        st.subheader("Pull Requests Created")
+        st.dataframe(pull_requests)
+
+    # Starred Repositories
+    if st.sidebar.button('⭐ Fetch Starred Repos'):
+        starred_repos = conn.get_user_starred_repos(github_username)
+        st.subheader("Starred Repositories")
+        st.dataframe(starred_repos)
+
+    # Followers
+    if st.sidebar.button('👥 Fetch Followers'):
+        followers = conn.get_user_followers(github_username)
+        st.subheader("Followers")
+        st.dataframe(followers)
+
+    # Following
+    if st.sidebar.button('👣 Fetch Following'):
+        following = conn.get_user_following(github_username)
+        st.subheader("Following")
+        st.dataframe(following)
+
+    # Gists
+    if st.sidebar.button('📜 Fetch Gists'):
+        gists = conn.get_user_gists(github_username)
+        st.subheader("Gists")
+        st.dataframe(gists)
+
+    # Organizations
+    if st.sidebar.button('🏢 Fetch Organizations'):
+        orgs = conn.get_user_organizations(github_username)
+        st.subheader("Organizations")
+        st.dataframe(orgs)
 
 else:
     st.sidebar.warning("Please enter a GitHub username.")
