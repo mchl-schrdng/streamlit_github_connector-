0cbf017793081626c136996395f7d1dd2acf90c3
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-28 10:42:09 +0200

    Update github_connector.py

--- src/github_connector.py
+++ src/github_connector.py
@@ -1,14 +1,14 @@
+# src/github_connector.py
+
 from streamlit.connections import ExperimentalBaseConnection
 import streamlit as st
 
 class GitHubConnection(ExperimentalBaseConnection):
 
     def _connect(self, **kwargs):
-        # Retrieve the GitHub token from Streamlit's secrets
         self.token = st.secrets['github']['token']
         return self
 
     def get_user_activity(self, username):
-        # Here, you'd use the GitHub API to fetch the user's activity
-        # This method should return the desired data or handle any API-specific logic
+        # Implement the logic to fetch user activity from GitHub
         pass
