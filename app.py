490ee143e6c021c671614388a85a925fb9d5cd69
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-28 10:44:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@
 import streamlit as st
 from src.github_connector import GitHubConnection
 
-# Create an instance of the GitHubConnection
-conn = GitHubConnection()
+# Create an instance of the GitHubConnection with a connection name
+conn = GitHubConnection("github")
 
 # Use the connection to fetch user's GitHub activity
-user_activity = conn.get_user_activity("your_github_username")
+user_activity = conn.get_user_activity("mchl-schrdng")
 st.write(user_activity)
