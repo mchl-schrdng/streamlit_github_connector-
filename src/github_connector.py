9c524e54fd2f3431220551c4d7deb4c1fb165054
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-28 10:56:22 +0200

    Update github_connector.py

--- src/github_connector.py
+++ src/github_connector.py
@@ -1,17 +1,24 @@
-# src/github_connector.py
-
 from streamlit.connections import ExperimentalBaseConnection
-import streamlit as st
+import requests
 
 class GitHubConnection(ExperimentalBaseConnection):
-
     def __init__(self, connection_name):
         super().__init__(connection_name)
+        self.token = st.secrets["github"]["token"]
 
-    def _connect(self, **kwargs):
-        self.token = st.secrets['github']['token']
+    def _connect(self):
+        # No persistent connection is established with GitHub API, so we just return self
         return self
 
     def get_user_activity(self, username):
-        # Implement the logic to fetch user activity from GitHub
-        pass
+        url = f"https://api.github.com/users/{username}/events/public"
+        headers = {
+            "Authorization": f"token {self.token}",
+            "Accept": "application/vnd.github.v3+json"
+        }
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            st.error(f"Error {response.status_code}: {response.text}")
+            return []
