0411d1a6b8c28c2c5b4e9b553e0128a4ee0399a3
Mchl <73759636+mchl-schrdng@users.noreply.github.com>
2023-07-28 13:30:29 +0200

    Update github_connector.py

--- src/github_connector.py
+++ src/github_connector.py
@@ -4,16 +4,17 @@ import requests
 
 class GitHubConnection(ExperimentalBaseConnection):
 
+    BASE_URL = "https://api.github.com"
+
     def __init__(self, connection_name):
         super().__init__(connection_name)
         self.token = st.secrets["github"]["token"]
 
     def _connect(self):
-        # No persistent connection is established with GitHub API, so we just return self
         return self
 
-    def get_user_activity(self, username):
-        url = f"https://api.github.com/users/{username}/events/public"
+    def _make_request(self, endpoint):
+        url = f"{self.BASE_URL}{endpoint}"
         headers = {
             "Authorization": f"token {self.token}",
             "Accept": "application/vnd.github.v3+json"
@@ -24,3 +25,36 @@ class GitHubConnection(ExperimentalBaseConnection):
         else:
             st.error(f"Error {response.status_code}: {response.text}")
             return []
+
+    def get_user_profile(self, username):
+        return self._make_request(f"/users/{username}")
+
+    def get_user_activity(self, username):
+        return self._make_request(f"/users/{username}/events/public")
+
+    def get_user_repositories(self, username):
+        return self._make_request(f"/users/{username}/repos")
+
+    def get_user_issues(self, username):
+        return self._make_request(f"/search/issues?q=author:{username}")
+
+    def get_user_pull_requests(self, username):
+        return self._make_request(f"/search/issues?q=author:{username} type:pr")
+
+    def get_user_starred_repos(self, username):
+        return self._make_request(f"/users/{username}/starred")
+
+    def get_user_followers(self, username):
+        return self._make_request(f"/users/{username}/followers")
+
+    def get_user_following(self, username):
+        return self._make_request(f"/users/{username}/following")
+
+    def get_user_gists(self, username):
+        return self._make_request(f"/users/{username}/gists")
+
+    def get_user_organizations(self, username):
+        return self._make_request(f"/users/{username}/orgs")
+
+    def get_repo_languages(self, owner, repo):
+        return self._make_request(f"/repos/{owner}/{repo}/languages")
